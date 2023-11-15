21d2b1a0e3a21f51087b0f44915a84d18d5c1291
xibrahimmasood <128328616+xibrahimmasood@users.noreply.github.com>
2023-11-15 17:08:37 +0530

    SmallLetterToCapitalLetter

---
+++ SmalltoCapitalLetter.java
@@ -0,0 +1,16 @@
+package ifelse;
+
+public class SmalltoCapitalLetter {
+	public static void main(String[] args) {
+		
+
+	
+	char ch;
+	ch = 'p';
+
+	ch = (char)(ch - 32); //(ch+32) for capital to small
+	System.out.println(ch);
+	
+	}
+
+}
