5135abd1bd69b9d8ed355c7d039e0936ca4a7cf6
xibrahimmasood <128328616+xibrahimmasood@users.noreply.github.com>
2023-11-21 05:02:01 -0500

    Loops in Java
    Examples for:
    1. do while
    2. while
    3. while

---
+++ Demo.java
@@ -0,0 +1,26 @@
+package ifelse;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		int i = 1;
+		do {
+			System.out.println("do while "+i);
+			i+=1;
+		} while(i<=5);
+		
+		int j = 1;
+		while(j<=10) {
+		System.out.println("while " +j);
+		j++;
+		}
+		for(int k=1;k<=5;k++) {
+			System.out.println("For Loop "+k);
+		}
+		
+		System.out.println("Program ends");
+		
+
+	}
+
+}
