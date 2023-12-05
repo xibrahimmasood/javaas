41cae281613151a01df6992434828c8580032cb5
xibrahimmasood <128328616+xibrahimmasood@users.noreply.github.com>
2023-12-05 16:38:18 +0530

    Add files via upload

--- ElementsOfAandBinC.java
+++ ElementsOfAandBinC.java
@@ -3,17 +3,17 @@ package ifelse;
 public class ElementsOfAandBinC {
 
 		public static void main(String[] args) {
-			int a[] = {10,20,30,40,50,60,70,80};
-			int b[] = {1,2,3,4,5,6,7,8};
+			int a[] = {10,20,30,40,50,60};
+			int b[] = {100,200,300,400,500,600};
 			int c[] = new int[a.length + b.length];
 			
 			for(int i=0;i<a.length;i++) {
 				c[i] = a[i];
 			}
 			for(int i=a.length;i<c.length;i++) {
-				c[i] = b[i - a.length];
-			}
-			
+				c[i] = b[i - a.length]; //As were starting from a.length which is 6,so i is 6
+			}							//so on right we want to start from 1 so we minus a.length
+										//ex. 6: 6-6 0 ; 7: 7-5 2.........
 			System.out.println("A array elements: ");
 			for(int i=0;i<a.length;i++) {
 				System.out.print(a[i]+" ");
